60094b660a0da25847ae545b2757a75f98625899
chuategiaitich <nguyenminhtue148@gmail.com>
2026-09-23 13:03:12 +0700

    a

--- firmware/build/Debug/cmake/stm32cubemx/cmake_install.cmake
+++ firmware/build/Debug/cmake/stm32cubemx/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/MM_Control/firmware/cmake/stm32cubemx
+# Install script for directory: D:/Project_Kicad/MM_Control/firmware/cmake/stm32cubemx
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,6 +40,6 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "D:/MM_Control/firmware/build/Debug/cmake/stm32cubemx/install_local_manifest.txt"
+  file(WRITE "D:/Project_Kicad/MM_Control/firmware/build/Debug/cmake/stm32cubemx/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
